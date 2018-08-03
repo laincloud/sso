1f993221c0d463dc68a1a448a5d98ff62ba81504
wang522 <wangyifan1210@163.com>
2018-08-03 16:02:36 +0800

    add app modify apis

--- ssolib/server.go
+++ ssolib/server.go
@@ -160,7 +160,8 @@ func (s *Server) ListenAndServe(addr string, addHandlers AddHandles) error {
 
 	s.AddRestfulResource("/api/users/:username", "UserResource", UserResource{})
 	s.AddRestfulResource("/api/me", "MeResource", MeResource{})
-	s.AddRestfulResource("/api/apps", "AppResource", AppResource{})
+	s.AddRestfulResource("/api/apps", "AppsResource", AppsResource{})
+	s.AddRestfulResource("/api/app/:id", "AppResource", AppResource{})
 	s.AddRestfulResource("/api/groups", "GroupsResource", GroupsResource{})
 	s.AddRestfulResource("/api/groups/:groupname", "GroupResource", GroupResource{})
 	s.AddRestfulResource("/api/groups/:groupname/members/:username",
